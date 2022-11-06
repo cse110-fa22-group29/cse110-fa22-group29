ec305e667da5219efa8bc961c2f6ee5df2656cca
Arthur Lu <learthurgo@gmail.com>
2022-11-06 05:46:23 +0000

    switch unit test framework to jest, remove framework unittest, add helper module to create unit testing environment

---
+++ testenv/testenv.test.js
@@ -0,0 +1,86 @@
+const {environment} = require("./testenv.js");
+
+function saveToLocal (k, v) {
+	window.localStorage.setItem(k, v);
+}
+
+function getFromLocal (k) {
+	return window.localStorage.getItem(k);
+}
+
+function removeFromLocal (k) {
+	window.localStorage.removeItem(k);
+}
+
+function clearLocal () {
+	window.localStorage.clear();
+} 
+
+describe("test localStorage mock", () => {
+	test("test save and fetch", () => {
+		let window = environment();
+		saveToLocal("testkey1", "testvalue1");
+		saveToLocal("testkey2", "testvalue2");
+		saveToLocal("testkey3", "testvalue3");
+		saveToLocal("testkey4", "testvalue4");
+
+		expect(getFromLocal("testkey1")).toBe("testvalue1");
+		expect(getFromLocal("testkey2")).toBe("testvalue2");
+		expect(getFromLocal("testkey3")).toBe("testvalue3");
+		expect(getFromLocal("testkey4")).toBe("testvalue4");
+
+		saveToLocal("testkey6", "testvalue5");
+		expect(getFromLocal("testkey6")).toBe("testvalue5");
+	});
+
+	test("test delete and fetch", () => {
+		let window = environment();
+		saveToLocal("testkey1", "testvalue1");
+		saveToLocal("testkey2", "testvalue2");
+		saveToLocal("testkey3", "testvalue3");
+		saveToLocal("testkey4", "testvalue4");
+
+		removeFromLocal("testkey3");
+
+		expect(getFromLocal("testkey1")).toBe("testvalue1");
+		expect(getFromLocal("testkey2")).toBe("testvalue2");
+		expect(getFromLocal("testkey3")).toBe(null);
+		expect(getFromLocal("testkey4")).toBe("testvalue4");
+
+		removeFromLocal("testkey1");
+
+		expect(getFromLocal("testkey1")).toBe(null);
+		expect(getFromLocal("testkey2")).toBe("testvalue2");
+		expect(getFromLocal("testkey3")).toBe(null);
+		expect(getFromLocal("testkey4")).toBe("testvalue4");
+
+		removeFromLocal("testkey4");
+		
+		expect(getFromLocal("testkey1")).toBe(null);
+		expect(getFromLocal("testkey2")).toBe("testvalue2");
+		expect(getFromLocal("testkey3")).toBe(null);
+		expect(getFromLocal("testkey4")).toBe(null);
+
+		removeFromLocal("testkey2");
+		
+		expect(getFromLocal("testkey1")).toBe(null);
+		expect(getFromLocal("testkey2")).toBe(null);
+		expect(getFromLocal("testkey3")).toBe(null);
+		expect(getFromLocal("testkey4")).toBe(null);
+	});
+
+	test("test delete and fetch", () => {
+		let window = environment();
+		saveToLocal("testkey1", "testvalue1");
+		saveToLocal("testkey2", "testvalue2");
+		saveToLocal("testkey3", "testvalue3");
+		saveToLocal("testkey4", "testvalue4");
+
+		clearLocal();
+
+		expect(getFromLocal("testkey1")).toBe(null);
+		expect(getFromLocal("testkey2")).toBe(null);
+		expect(getFromLocal("testkey3")).toBe(null);
+		expect(getFromLocal("testkey4")).toBe(null);
+	});
+});
