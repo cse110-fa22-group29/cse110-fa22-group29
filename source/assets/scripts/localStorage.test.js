b85a73400dacfbcc16eed9703fa75386a1a59962
Arthur Lu <learthurgo@gmail.com>
2022-11-10 22:33:47 +0000

    add another unit test to localStorage.test.js, reflect fixes to packages.json from main

--- source/assets/scripts/localStorage.test.js
+++ source/assets/scripts/localStorage.test.js
@@ -13,15 +13,37 @@ describe("test app localStorage interaction", () => {
 		assert.deepEqual(getReviewsFromStorage(), []);
 	});
 	it("store one then get", () => {
-		reviews = [{
+		let reviews = [{
 			"imgSrc": "sample src",
 			"imgAlt": "sample alt",
 			"mealName": "sample name",
 			"restaurant": "sample restaurant",
-			"rating": 5
+			"rating": 5,
+			"tags": ["tag 1", "tag  2", "tag 3"]
 		}];
 
 		saveReviewsToStorage(reviews);
 		assert.deepEqual(getReviewsFromStorage(), reviews);
-	})
+	});
+	it("repeated store one more and get", () => {
+		saveReviewsToStorage(reviews);
+		assert.deepEqual(getReviewsFromStorage(), reviews);
+
+		for(let i = 0; i < 1000; i++){
+			reviews = getReviewsFromStorage();
+			
+			reviews.push(
+				{
+					"imgSrc": `sample src ${i}`,
+					"imgAlt": `sample alt ${i}`,
+					"mealName": `sample name ${i}`,
+					"restaurant": `sample restaurant ${i}`,
+					"rating": i,
+					"tags": [`tag ${3*i}`, `tag ${3*i + 1}`, `tag ${3*i + 2}`]
+				}
+			)
+			saveReviewsToStorage(reviews);
+			assert.deepEqual(getReviewsFromStorage(), reviews);
+		}
+	});
 });
