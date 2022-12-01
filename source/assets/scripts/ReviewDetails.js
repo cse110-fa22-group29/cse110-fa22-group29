83141a3cce1179e80a5db269377bc426bdb4aa5f
Arthur Lu <learthurgo@gmail.com>
2022-11-30 18:06:29 -0800

    not a good way to do this
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- source/assets/scripts/ReviewDetails.js
+++ source/assets/scripts/ReviewDetails.js
@@ -14,36 +14,45 @@ function init(){
  * Populates the relevant data to the details from local storage review
  */
 function setupInfo(){
+	let updateBtn = document.getElementById("update-btn");
 	let currID = JSON.parse(sessionStorage.getItem("currID"));
 	let currReview = getReviewFromStorage(currID);
-	
-	//meal image
-	let mealImg = document.getElementById("d-meal-img");
-	mealImg.setAttribute("src",currReview["mealImg"]);
-	mealImg.addEventListener("error", function(e) {
-		mealImg.setAttribute("src", "./assets/images/default_plate.png");
-		e.onerror = null;
-	});
+	let tagContainer = document.getElementById("tag-container-form");
 
-	//meal name
-	let mealLabel = document.getElementById("d-meal-name");
-	mealLabel.innerHTML = currReview["mealName"];
+	//Set value of each input element to current's values
+	document.getElementById("d-meal-img").defaultValue = currReview["mealImg"];
+	document.getElementById("d-meal-name").defaultValue = currReview["mealName"];
+	document.getElementById("d-comments").textContent = currReview["comments"];
+	document.getElementById("s" + `${currReview["rating"]}`).checked = true;
+	document.getElementById("d-restaurant").defaultValue = currReview["restaurant"];
 
-	//restaurant name
-	let restaurantLabel = document.getElementById("d-restaurant");
-	restaurantLabel.innerHTML = currReview["restaurant"];
-	
-	//comments
-	let comments = document.getElementById("d-comments");
-	comments.innerText = currReview["comments"];
+	// set inputs uneditable or hidden
+	document.getElementById('d-meal-name').setAttribute('readonly', true);
+	document.getElementById('d-meal-name').style.borderBottom = "none";
+	document.getElementById('d-meal-name').style.fontFamily = "Century Gothic";
+	document.getElementById('d-meal-name').style.fontSize = "2em";
+	document.getElementById('d-meal-name').style.textAlign = "center";
+	document.getElementById('d-meal-name').style.fontWeight = "bold";
+	document.getElementById('d-restaurant').setAttribute('readonly', true);
+	document.getElementById('d-restaurant').style.borderBottom = "none";
+	document.getElementById('d-restaurant').style.fontFamily = "Century Gothic";
+	document.getElementById('d-restaurant').style.fontSize = "1.5em";
+	document.getElementById('d-restaurant').style.textAlign = "center";
+	document.getElementById('d-meal-name').style.fontWeight = "bold";
+	document.getElementById('select').classList.add("hidden");
+	document.getElementById('source').classList.add("hidden");
+	document.querySelector('.rating').classList.add("hidden");
+	document.getElementById('tag-form').classList.add("hidden");
+	document.getElementById('tag-add-btn').classList.add("hidden");
+	document.getElementById('save-btn').classList.add("hidden");
+	document.getElementById('home-btn').classList.add("hidden");
 
 	//rating
 	let starsImg = document.getElementById("d-rating");
 	starsImg.setAttribute("src", "./assets/images/"+currReview["rating"]+"-star.svg");
 	starsImg.setAttribute("alt", currReview["rating"] +" stars");
-	
+
 	//tags
-	let tagContainer = document.getElementById("d-tags");
 	if(currReview["tags"]){
 		for (let i = 0; i < currReview["tags"].length; i++) {
 			let newTag = document.createElement("label");
@@ -52,6 +61,37 @@ function setupInfo(){
 			tagContainer.append(newTag);
 		}
 	}
+
+	//meal image
+	let mealImg = document.getElementById("d-meal-img");
+	mealImg.setAttribute("src",currReview["mealImg"]);
+	mealImg.addEventListener("error", function(e) {
+		mealImg.setAttribute("src", "./assets/images/default_plate.png");
+		e.onerror = null;
+	});
+
+	/*
+	* change the input source of the image between local file and URL 
+	* depending on user's selection
+	*/
+	let select = document.getElementById("select");
+	select.addEventListener("change", function() {
+		const input = document.getElementById("source");
+	
+		if (select.value == "file") {
+			input.innerHTML = `
+			Source:
+			<input type="file" accept="image/*" id="mealImg" name="mealImg">
+			`;
+		}
+		//TODO: change to photo taking for sprint 3
+		else {
+			input.innerHTML = `
+			Source:
+			<input type="text" id="mealImg" name="mealImg">
+			`;
+		}
+	});
 }
 
 /**
@@ -73,147 +113,5 @@ function setupDelete(){
  * Sets up update button to reveal form and update info in storage and the current page 
  */
 function setupUpdate(){
-	let updateBtn = document.getElementById("update-btn");
-	let currID = JSON.parse(sessionStorage.getItem("currID"));
-	let currReview = getReviewFromStorage(currID);
-	let form = document.getElementById("new-food-entry");
-	let updateDiv = document.getElementById("update-form");
-	updateBtn.addEventListener("click", function(){
-		//update function
-
-		updateDiv.classList.remove("hidden");
-
-		let tagContainer = document.getElementById("tag-container-form");
-
-		//Set value of each input element to current's values
-		document.getElementById("mealImg").defaultValue = currReview["mealImg"];
-		document.getElementById("mealName").defaultValue = currReview["mealName"];
-		document.getElementById("comments").textContent = currReview["comments"];
-		document.getElementById("s" + `${currReview["rating"]}`).checked = true;
-		document.getElementById("restaurant").defaultValue = currReview["restaurant"];
-
-		//Set used to track tags and ensure no duplicates
-		let tagSet = new Set();
-
-		if(currReview["tags"]){
-			while (tagContainer.firstChild) {
-				tagContainer.removeChild(tagContainer.firstChild);
-			}
-			let tagSetVal;
-			for (let i = 0; i < currReview["tags"].length; i++) {
-				tagSetVal = currReview["tags"][i].toLowerCase()
-				tagSet.add(tagSetVal);
-				let newTag = document.createElement("label");
-				newTag.setAttribute("class","tag");
-				newTag.innerHTML = currReview["tags"][i];
-				newTag.addEventListener("click",()=> {
-					tagContainer.removeChild(newTag);
-					tagSet.delete(tagSetVal);
-				});
-				tagContainer.append(newTag);
-			}
-		}
-
-		/*
-		* change the input source of the image between local file and URL 
-		* depending on user's selection
-		*/
-		let select = document.getElementById("select");
-		select.addEventListener("change", function() {
-			const input = document.getElementById("source");
-		
-			if (select.value == "file") {
-				input.innerHTML = `
-				Source:
-				<input type="file" accept="image/*" id="mealImg" name="mealImg">
-				`;
-			}
-			//TODO: change to photo taking for sprint 3
-			else {
-				input.innerHTML = `
-				Source:
-				<input type="text" id="mealImg" name="mealImg">
-				`;
-			}
-		});
-
-		//addressing sourcing image from local file
-		let imgDataURL = "";
-		document.getElementById("mealImg").addEventListener("change", function() {
-			console.log("reading used");
-			const reader = new FileReader();
-			
-			//store image data URL after successful image load
-			reader.addEventListener("load", ()=>{
-				imgDataURL = reader.result;
-			}, false);
-			
-			//convert image file into data URL for local storage
-			reader.readAsDataURL(document.getElementById("mealImg").files[0]);
-		});
-
-		
-		//Take formdata values as newData when submit
-		form.addEventListener("submit", function(){
-			/*
-			*  User submits the form for their review.
-			*  We create reviewCard data, replace in storage, and update tags
-			*/
-			let formData = new FormData(form);
-			let newData = {};
-			//iterate through formData and add to newData
-			for (let [key, value] of formData) {
-				console.log(`${key}`);
-				console.log(`${value}`);
-				if (`${key}` !== "tag-form") {
-					newData[`${key}`] = `${value}`;
-				}
-				//Account for the case where image is not updated
-				if (`${key}` === "mealImg" && document.getElementById("mealImg").value === "") {
-					newData["mealImg"] = currReview["mealImg"];
-				}
-				else if (`${key}` === "mealImg" && select.value == "file") {
-					newData["mealImg"] = imgDataURL;
-				}
-			}
-			newData["tags"] = [];
-		
-			let tags = document.querySelectorAll(".tag");
-			for(let i = 0; i < tags.length; i ++) {
-				newData["tags"].push(tags[i].innerHTML);
-				tagContainer.removeChild(tags[i]);
-			}
-
-			newData["reviewID"] = currID;
-			
-			updateReviewToStorage(currID, newData);
-
-			updateDiv.classList.add("hidden");
-
-		});
-
-		//adding tag to form functionality
-		let tagAddBtn = document.getElementById("tag-add-btn");
-		tagAddBtn.addEventListener("click", ()=> {
-			let tagField = document.getElementById("tag-form");
-			if (tagField.value.length > 0) {
-				let tagSetVal = tagField.value.toLowerCase();
-				if (!tagSet.has(tagSetVal)){
-					let tagLabel = document.createElement("label");
-					tagLabel.innerHTML = tagField.value;
-					tagLabel.setAttribute("class","tag");
-					tagSet.add(tagSetVal);
-					tagLabel.addEventListener("click",()=> {
-						tagContainer.removeChild(tagLabel);
-						tagSet.delete(tagSetVal);
-					});
-			
-					tagContainer.append(tagLabel);
-				} else {
-					window.alert("No duplicate tags allowed");
-				}
-				tagField.value = "";
-			}
-		});
-	});
+	
 }
