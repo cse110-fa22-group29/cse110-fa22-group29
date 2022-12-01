d95f0036ac2941f8e7eab33a4b2ea40fd6612476
Arthur Lu <learthurgo@gmail.com>
2022-12-01 07:36:19 +0000

    clean up some styling

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -44,9 +44,7 @@ function initFormHandler() {
 const registerServiceWorker = async () => {
 	if ("serviceWorker" in navigator) {
 		try {
-			const registration = await navigator.serviceWorker.register("./sw.js", {
-				scope: "./",
-			});
+			const registration = await navigator.serviceWorker.register("./sw.js", {scope: "./"});
 		} catch (error) {
 			console.error(`Registration failed with ${error}`);
 		}
